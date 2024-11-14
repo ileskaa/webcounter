b5bd698f02ebaec3e2f1d378cce462ef2c7df02b
ileskaa <akseli.ingervo@gmail.com>
2024-11-14 10:57:07 +0200

    Complete exercise 4 of week 3

--- src/app.py
+++ src/app.py
@@ -1,19 +1,29 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from counter import Counter
 
 app = Flask(__name__)
 cnt = Counter()
 
+
 @app.route("/")
 def index():
     return render_template("index.html", value=cnt.value)
+
 
 @app.route("/increment", methods=["POST"])
 def increment():
     cnt.increase()
     return redirect("/")
 
+
 @app.route("/reset", methods=["POST"])
 def reset():
     cnt.reset()
+    return redirect("/")
+
+
+@app.route("/set-value", methods=["POST"])
+def set_value():
+    value = request.form["value"]
+    cnt.set_value(value)
     return redirect("/")
